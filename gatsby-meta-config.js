49db773582a90a5c1351c74e5cbadc50a892da27
YoonHyukJun <gurwns1540@gmail.com>
2022-03-12 20:05:33 +0900

    init

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `dev_y Blog`,
+  description: `developer`,
+  author: `윤혁준`,
+  introduction: `개발자 윤혁준입니다.`,
+  siteUrl: `https://devy1540.github.io/`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `devy1540`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/icon.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    utterances: "devy1540/devy1540.github.io", // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: "",
+  },
+  share: {
+    facebookAppId: "", // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: "G-SW73FSL7LQ", // Add your google analytics tranking ID
+  // jennifer_front_id: "ed243e67", // Add your jennifer front tranking ID
+}
